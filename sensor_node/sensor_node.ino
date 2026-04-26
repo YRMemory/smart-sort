0164fd5688a31ce224c0a1494f1c1630ee98ab73
Anastasia Mozgovaya <147587830+anastasia-mozgovaya@users.noreply.github.com>
2026-04-27 01:31:22 +0300

    Прошивка

--- sensor_node/sensor_node.ino
+++ sensor_node/sensor_node.ino
@@ -1,4 +1,3 @@
-// --- ПОРТЫ ДАТЧИКОВ (Ультразвуковые) ---
 const int trigPlastic = 4; // Синий (ID: 1)
 const int echoPlastic = 5;
 
@@ -8,11 +7,9 @@ const int echoPaper = 7;
 const int trigOther = 11;  // Серый (ID: 3)
 const int echoOther = 12;
 
-// --- НАСТРОЙКИ ---
 const int threshold = 11;       // Дистанция срабатывания (см)
 const int quietTime = 30;       // Пауза между опросами датчиков
-const long waitTimeout = 15000; // Тайм-аут ожидания 15 сек
-
+const long waitTimeout = 15000; // Ожидание 15 секунд
 void setup() {
   Serial.begin(9600);
   
@@ -25,7 +22,6 @@ void setup() {
 }
 
 void loop() {
-  // 1. Ждем ЛЮБУЮ команду от C#, чтобы начать слежение
   if (Serial.available() > 0) {
     String cmd = Serial.readStringUntil('\n');
     if (cmd.length() > 0) {
@@ -38,21 +34,21 @@ void waitForObject() {
   unsigned long startTime = millis();
   
   while (millis() - startTime < waitTimeout) {
-    // Датчик 1: СИНИЙ
+    //Датчик 1: СИНИЙ
     if (checkSensor(trigPlastic, echoPlastic)) {
       Serial.println("ID:1"); // Отправляем только ID
       return;
     }
     delay(quietTime);
 
-    // Датчик 2: ЖЕЛТЫЙ
+    //Датчик 2: ЖЕЛТЫЙ
     if (checkSensor(trigPaper, echoPaper)) {
       Serial.println("ID:2");
       return;
     }
     delay(quietTime);
 
-    // Датчик 3: СЕРЫЙ
+    //Датчик 3: СЕРЫЙ
     if (checkSensor(trigOther, echoOther)) {
       Serial.println("ID:3");
       return;
@@ -60,14 +56,11 @@ void waitForObject() {
     delay(quietTime);
   }
 
-  // Если за 15 секунд ничего не произошло
   Serial.println("ID:TIMEOUT");
 }
 
-// Утилита для проверки конкретного датчика
 bool checkSensor(int trig, int echo) {
   long distance = getDistance(trig, echo);
-  // Если дистанция меньше порога (но не 0)
   return (distance > 0 && distance <= threshold);
 }
 
@@ -78,7 +71,7 @@ long getDistance(int trig, int echo) {
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  long duration = pulseIn(echo, HIGH, 30000); // 30мс тайм-аут
+  long duration = pulseIn(echo, HIGH, 30000); 
   if (duration == 0) return 999;
   return duration * 0.034 / 2;
 }
